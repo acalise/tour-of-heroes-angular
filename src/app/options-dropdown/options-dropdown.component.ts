65108dc22eae4ee4735baf3156729914e8b0ead1
Calise <acalise@forbes.com>
2022-08-01 16:37:52 -0400

    Finished tour of heroes tutorial

---
+++ src/app/options-dropdown/options-dropdown.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-options-dropdown',
+  templateUrl: './options-dropdown.component.html',
+  styleUrls: ['./options-dropdown.component.scss']
+})
+export class OptionsDropdownComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
